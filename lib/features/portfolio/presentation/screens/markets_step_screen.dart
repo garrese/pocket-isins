007490dc52d00b1fcaa6cdd90800c775bd9c05a9
garrese <>
2026-04-02 00:00:00 +0200

    Dependencies fix

--- lib/features/portfolio/presentation/screens/markets_step_screen.dart
+++ lib/features/portfolio/presentation/screens/markets_step_screen.dart
@@ -6,6 +6,7 @@ import '../../../../core/network/market_data_service.dart';
 import '../../../../core/services/log/talker_provider.dart';
 import '../../data/portfolio_provider.dart';
 import 'additional_data_step_screen.dart';
+import 'registered_name_step_screen.dart';
 import 'wizard_bottom_actions.dart';
 
 class MarketsStepScreen extends ConsumerStatefulWidget {
